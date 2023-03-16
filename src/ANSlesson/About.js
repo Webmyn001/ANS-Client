f13afdb8a0dab127dbce96408e245df5302cdb2d
Webmyn001 <bellomuhyideen0001@gmail.com>
2023-03-16 16:52:43 -0700

    first

--- src/ANSlesson/About.js
+++ src/ANSlesson/About.js
@@ -10,22 +10,22 @@ function About() {
    <h1 className="text-center text-2xl sm:text-3xl pt-[7px] font-bold">About </h1>
 <div className="flex flex-col justify-center items-center" data-aos="flip-right">
          <div className="w-[350px] sm:w-[500px] md:w-[600px] ">
-            <h2 className="text-center p-4 font-semibold ">
+            <h2 className="text-center p-4  ">
           Bello Muhyideen Oladimeji, A Website Developer(MERN STACK) as well as a student at
           Obafemi Awolowo University, Ile-Ife, Nigeria.
-             <h2>He is dedicated, open-minded, and calm. He gets across to people and adjust to changes with ease.
+             <h2 className='pt-2'>He is dedicated, open-minded, and calm. He gets across to people and adjust to changes with ease.
               He believes that a person should work on developing 
               their professional skills and learning new things all the time.
              </h2>
           
-          <h2 className="pt-4 text-base">This website is purposely developed
+          <h2 className="pt-4 ">This website is purposely developed
            to be a learning platform for Agricultural students
            across the World to learn the essential knowledge on 
            Agricultural Genetics with simplified explanations.
 
            
            </h2>
-           <h2 className="pt-4 text-base">I hope you will find the website intresting, educative, and friendly. </h2>
+           <h2 className="pt-4 ">I hope you will find the website intresting, educative, and friendly. </h2>
            </h2>
 </div>
 </div>
