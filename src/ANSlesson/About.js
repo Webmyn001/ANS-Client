12f0d05e46600bc440807253cb81bad1cfd18548
Webmyn001 <bellomuhyideen0001@gmail.com>
2023-03-16 16:45:43 -0700

    first

--- src/ANSlesson/About.js
+++ src/ANSlesson/About.js
@@ -7,20 +7,25 @@ import "aos/dist/aos.css";
 function About() {
   return (
     <div className='bg-gradient-to-t min-h-screen text-[#7f030d] from-white to-[#fffadd] '>
-   <h1 className="text-center text-2xl sm:text-3xl pt-[7px] font-bold">About us</h1>
+   <h1 className="text-center text-2xl sm:text-3xl pt-[7px] font-bold">About </h1>
 <div className="flex flex-col justify-center items-center" data-aos="flip-right">
          <div className="w-[350px] sm:w-[500px] md:w-[600px] ">
             <h2 className="text-center p-4 font-semibold ">
-             Make It Halal Ramadan Relief program commitee wish to asist students all over Nigeria Institution 
-             to reduce suffering during Ramadan.
-             <h2> Give the Needy student opportunities to be able to eat healthy food during the month of mercy.</h2>
+          Bello Muhyideen Oladimeji, A Website Developer(MERN STACK) as well as a student at
+          Obafemi Awolowo University, Ile-Ife, Nigeria.
+             <h2>He is dedicated, open-minded, and calm. He gets across to people and adjust to changes with ease.
+              He believes that a person should work on developing 
+              their professional skills and learning new things all the time.
+             </h2>
           
-          <h2 className="pt-4 text-base">The Program started 1442 A.H and we were able to 
-          reach 25 students from different universities, 1443 A.H we were able to reach out to 50 students 
-          from different universities.
+          <h2 className="pt-4 text-base">This website is purposely developed
+           to be a learning platform for Agricultural students
+           across the World to learn the essential knowledge on 
+           Agricultural Genetics with simplified explanations.
+
+           
            </h2>
-           <h2 className="pt-4 text-base">This year we hope to increase the amount and the number of student that 
-           will benefit from our Ramadan Relief Program. </h2>
+           <h2 className="pt-4 text-base">I hope you will find the website intresting, educative, and friendly. </h2>
            </h2>
 </div>
 </div>
